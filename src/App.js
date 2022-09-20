a2c231aaab7b13ab1af213804060d1141cbb9fcb
bhavani <bhavanipanda654@gmail.com>
2022-09-20 16:44:58 +0530

    this is my first commit

--- src/App.js
+++ src/App.js
@@ -1,65 +1,78 @@
 import React from "react";
 import { Component } from "react";
-import Crossword from "@jaredreisinger/react-crossword";
+// import Crossword from "@jaredreisinger/react-crossword";
+import Crossword from "./myFiles/react-crossword";
 import "./styles.css";
 
 const data = {
   across: {
     1: {
-      clue: "abbreviated programming language",
-      answer: "JS",
-      row: 1,
-      col: 6
+      clue: "Sugar is one type of ",
+      answer: "CARBOHYDRATE",
+      row: 4,
+      col: 8
     },
     2: {
-      clue: "JS library for building UIs",
-      answer: "REACT",
-      row: 3,
-      col: 6
-    },
-    3: {
-      clue: "DOM bindings for React Router",
-      answer: "ROUTERDOM",
-      row: 5,
+      clue: "______ is a condition which results when insufficient food is eaten over a prolonged time.",
+      answer: "UNDERNUTRITION",
+      row: 6,
       col: 0
     },
-    4: {
-      clue: "JS runtime environment",
-      answer: "NODE",
-      row: 10,
+    3: {
+      clue: "This is also called fat/oil",
+      answer: "LIPIDN",
+      row: 9,
       col: 2
+    },
+    4: {
+      clue: "Essential for normal growth and nutrition and are required in small quantities in the diet.",
+      answer: "VITAMINF",
+      row: 11,
+      col: 5
+    },
+    5: {
+      clue: "These types of food help in body building & energy production.",
+      answer: "PROXMATEFOOD",
+      row: 14,
+      col: 1
+    },
+    6: {
+      clue: "It is an inorganic substance needed by the human body for good health",
+      answer: "MINERALS",
+      row: 17,
+      col: 5
     }
   },
   down: {
-    5: {
-      clue: "content management system",
-      answer: "WORDPRESS",
-      row: 0,
-      col: 0
-    },
-    6: {
-      clue: "enable config override",
-      answer: "REWIRED",
-      row: 4,
-      col: 4
-    },
     7: {
-      clue: "crossword creator",
-      answer: "JARED",
-      row: 1,
-      col: 6
+      clue: "They help to  build healthy bodies and make people more able to resist or fight diseases.",
+      answer: "PROTECTIVEFOOD",
+      row: 2,
+      col: 3
     },
     8: {
-      clue: "online publishing platform",
-      answer: "MEDIUM",
-      row: 5,
-      col: 8
+      clue: "______ is the condition results when excess food is eaten for a long period",
+      answer: "OVERNUTRITION",
+      row: 2,
+      col: 19
     },
     9: {
-      clue: "favourite code editor",
-      answer: "ATOM",
-      row: 2,
+      clue: "Helps in body building",
+      answer: "PROTEIN",
+      row: 3,
       col: 10
+    },
+    10: {
+      clue: "This is the science of food & its relationship to health.",
+      answer: "NUTRITION",
+      row: 9,
+      col: 7
+    },
+    11: {
+      clue: "._____ is the material that after ingestion helps in growth, nourishment, regeneration of cells & energy production for our day-to-day activity",
+      answer: "FOOD",
+      row: 11,
+      col: 12
     }
   }
 };
